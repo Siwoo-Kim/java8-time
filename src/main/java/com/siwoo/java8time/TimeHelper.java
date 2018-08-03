8fa713cba4319f40645290a163819de8cfe9b47a
skim327 <skim327@myseneca.ca>
2018-08-03 14:57:27 -0400

    first commit

--- src/main/java/com/siwoo/java8time/TimeHelper.java
+++ src/main/java/com/siwoo/java8time/TimeHelper.java
@@ -1,12 +1,15 @@
 package com.siwoo.java8time;
 
 import org.junit.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.util.Assert;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+
+import static org.junit.Assert.assertNotNull;
 
 public class TimeHelper {
 
@@ -26,11 +29,21 @@ public class TimeHelper {
         System.out.println("DateTime Formatted: " + dateTimeFormatter.format(dateTime));
     }
 
-    public static LocalDate parse(String string) {
+    public static void validateParseString(String string) {
         if(string != null && string.trim().length() > 0) {
-            return LocalDate.parse(string);
+            return;
         }
         throw new IllegalArgumentException("Not valid input " + string);
+    }
+
+    public static LocalDate parseDate(String string) {
+        validateParseString(string);
+        return LocalDate.parse(string);
+    }
+
+    public static LocalTime parseTime(String string) {
+        validateParseString(string);
+        return LocalTime.parse(string);
     }
 
     @Test
@@ -38,6 +51,8 @@ public class TimeHelper {
         TimeHelper.display(LocalDate.now());
         TimeHelper.display(LocalTime.now());
         TimeHelper.display(LocalDateTime.now());
+
+        assertNotNull(TimeHelper.parseDate("1989-03-04"));
     }
 
 }
